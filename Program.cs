1f66663d5e9ebf5197e37c43154b66da632f96a2
Oleg <oleg.kurkov.85@gmail.com>
2022-12-22 14:09:44 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace _13_6_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "C:\\Users\\Kurkov_o\\Downloads\\Text1.txt";///путь к файлу
+            string readText = File.ReadAllText(filePath);
+            // Производительность вставки в обычный список
+            var stopWatch1 = Stopwatch.StartNew();
+            List<string> List = new List<string>();
+            foreach (string line in readText.Split(new char[] { '\n', '\r', ' ' }))
+            {
+                List.Add(line);
+            }
+            Console.WriteLine(stopWatch1.Elapsed.TotalMilliseconds);
+
+            // Производительность вставки в связанный список
+            var stopWatch2 = Stopwatch.StartNew();
+            LinkedList<string> LinkedList = new LinkedList<string>();
+            foreach (string line in readText.Split(new char[] { '\n', '\r', ' ' }))
+            {
+                LinkedList.AddLast(line);
+            }
+            Console.WriteLine(stopWatch2.Elapsed.TotalMilliseconds);
+            Console.ReadKey();
+        }
+    }
+}
